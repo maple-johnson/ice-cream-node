089c900d0d63e8e26b4c35008db6d3e9d0db2748
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-02-13 15:24:02 -0800

    Added order summary and page for summary

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ app.get('/', (req, res) => {
      res.render('home');
 });
 
+app.post('/thankyou', (req, res) => {
+     
+     const order = {
+          name: req.body.name,
+          email: req.body.email,
+          flavor: req.body.flavor,
+          cone: req.body.cone,
+          toppings: req.body.toppings,
+          comment: req.body.comment
+     };
+
+     console.log(order);
+     res.render('confirm', { order });
+})
+
 app.listen(PORT, () => {
      console.log(`Server is running at http://localhost:${PORT}`);
 });
